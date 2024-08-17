0e7176cb2881251292191d5ddf33e137ffa41e9f
zzq12345 <zzqzhj@126.com>
2024-08-17 19:35:03 +0800

    Update aioule.js

--- js/aioule.js
+++ js/aioule.js
@@ -1,6 +1,6 @@
 var rule = {
   title: '爱欧乐影院',
-  host: 'https://www.iole.tv',
+  host: 'https://www.olehdtv.com/',
   url: '/vodshow/fyclass--------fypage---.html',
   searchUrl: '/vodsearch/page/fypage/wd/*.html',
   class_parse: '.top_nav li;a&&Text;a&&href;.*/(.*?).html',
